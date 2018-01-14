7c57fa113884c4822a5fed2ee47b060a45893e9e
rupesh <32704769+rupeshsmorajkar@users.noreply.github.com>
2018-01-14 18:02:59 +1100

    removed firebase config

--- myshopuiangular/src/environments/environment.prod.ts
+++ myshopuiangular/src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyBhwqL8zbLvpxlqJ9UiDQCKcmSY_0TnSRk",
-    authDomain: "oshop-68414.firebaseapp.com",
-    databaseURL: "https://oshop-68414.firebaseio.com",
-    projectId: "oshop-68414",
-    storageBucket: "",
-    messagingSenderId: "411658412327"
-  }
+  production: true
 };
